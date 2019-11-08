f018b4400f6347689bc47153ed6791a42b808a92
Fulvio Farina <fulviofarina@gmail.com>
2019-11-08 18:05:15 -0400

    v1

--- SPWeb/SPWeb/WebForm.aspx.cs
+++ SPWeb/SPWeb/WebForm.aspx.cs
@@ -1,22 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
 using System.Web.UI.WebControls;
 
 namespace SPWeb
 {
     public partial class WebForm : System.Web.UI.Page
     {
-
-      
-
-
-       
-        private  ds dataset = null;
-        private  localhost.SPWS client = null;
+        private localhost.SPWS client = null;
+        private ds dataset = null;
+        protected void ListBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
 
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -25,11 +19,9 @@ namespace SPWeb
 
             string from = string.Empty;
             string desiredSKU = string.Empty;
-
             string all = "*";
 
-            string  refreshfromWebRts = string.Empty;
-
+            string refreshfromWebRts = string.Empty;
             string refreshfromWebTxs = string.Empty;
 
             if (IsPostBack)
@@ -48,40 +40,24 @@ namespace SPWeb
                     desiredSKU = string.Empty;
                     refreshfromWebTxs = string.Empty;
                 }
-              
-             
-            }
-          
-                dataset = new ds();
-                client = new localhost.SPWS();
-
-           
-
-                DataTable dt = client.LoadRates(from, string.Empty, string.Empty, refreshfromWebRts);
-                DataTable destiny = dataset.Rates;
-                TableTools.Merge(dt, destiny);
-
-            if (!IsPostBack)
-            {
-                currencyNodes = dataset.Rates.SelectDistinctFrom();
             }
 
+            dataset = new ds();
+            client = new localhost.SPWS();
 
+            DataTable dt = client.LoadRates(from, string.Empty, string.Empty, refreshfromWebRts);
+            DataTable destiny = dataset.Rates;
+            TableTools.Merge(dt, destiny);
 
             DataTable txs = client.LoadTransactions(from, desiredSKU, string.Empty, string.Empty, refreshfromWebTxs);
-                 destiny = dataset.Transactions;
-                TableTools.Merge(txs, destiny);
+            destiny = dataset.Transactions;
+            TableTools.Merge(txs, destiny);
 
-            if (!IsPostBack)
-            {
-                skus = dataset.Transactions.SelectDistinctSKU();
-            }
             //load totals
-             txs = client.LoadTransactions(from, desiredSKU, "y", string.Empty, "n");
+            txs = client.LoadTransactions(from, desiredSKU, "y", string.Empty, "n");
 
             destiny = dataset.TransactionsTotals;
             TableTools.Merge(txs, destiny);
-
 
             object binding = dataset.Rates;
             GridView theGrid = this.GridView1;
@@ -95,77 +71,46 @@ namespace SPWeb
             theGrid = this.GridView2;
             bind(ref binding, ref theGrid);
 
-      
+            if (!IsPostBack)
+            {
+                currencyNodes = dataset.Rates.SelectDistinctFrom();
+                skus = dataset.Transactions.SelectDistinctSKU();
+            }
 
             if (!IsPostBack)
             {
-                this.ListBox2.Items.Clear();
-                this.ListBox2.Items.Add(all);
+                ListBox lbox = this.ListBox2;
+                fillBox(currencyNodes, from, all, ref lbox);
 
-                foreach (var item in currencyNodes)
-                {
-                    this.ListBox2.Items.Add(item);
-                }
-                if (string.IsNullOrEmpty(from))
-                {
-                    this.ListBox2.Items.FindByText(all).Selected = true;
-                }
-                else
-                {
-                    this.ListBox2.Items.FindByText(from).Selected = true;
-                }
-
-                this.ListBox3.Items.Clear();
-                this.ListBox3.Items.Add(all);
-
-                foreach (var item in skus)
-                {
-                    this.ListBox3.Items.Add(item);
-                }
-                if (string.IsNullOrEmpty(desiredSKU))
-                {
-                    this.ListBox3.Items.FindByText(all).Selected = true;
-                }
-                else
-                {
-                    this.ListBox3.Items.FindByText(desiredSKU).Selected = true;
-                }
-
-
+                fillBox(skus, desiredSKU, all, ref lbox);
             }
-
-
         }
-
-       
-
-        /*
-        private void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            ListBox ls = sender as ListBox;
-
-            DataTable dt = client.LoadRates(ls.SelectedItem.Text, string.Empty, string.Empty, "n");
-
-            DataTable destiny = dataset.Rates;
-            TableTools.Merge(dt, destiny);
-
-            object binding = destiny;
-            GridView theGrid = this.GridView1;
-            bind(ref binding, ref theGrid);
-        }
-        */
 
         private static void bind(ref object binding, ref GridView theGrid)
         {
             theGrid.DataSource = binding;
-            //  this.GridView1.DataMember = dataset.Rates.TableName;
+            // this.GridView1.DataMember = dataset.Rates.TableName;
             theGrid.DataBind();
             theGrid.Visible = true;
         }
 
-        protected void ListBox2_SelectedIndexChanged(object sender, EventArgs e)
+        private static void fillBox(string[] nodes, string selectTag, string allTag, ref ListBox lbox)
         {
+            lbox.Items.Clear();
+            lbox.Items.Add(allTag);
 
+            foreach (var item in nodes)
+            {
+                lbox.Items.Add(item);
+            }
+            if (string.IsNullOrEmpty(selectTag))
+            {
+                lbox.Items.FindByText(allTag).Selected = true;
+            }
+            else
+            {
+                lbox.Items.FindByText(selectTag).Selected = true;
+            }
         }
     }
 }
